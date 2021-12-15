9e585369bc13b443de02c49037c9eacfdcd00ee9
初林 <15944192143@126.com>
2021-12-15 16:25:23 +0800

    ts

--- part5/src/modules/Snake.ts
+++ part5/src/modules/Snake.ts
@@ -11,6 +11,7 @@ class Snake{
     this.head = document.querySelector("#snake > div") as HTMLElement
     this.bodies = document.getElementById("snake")!.getElementsByTagName("div")
     this.element = document.getElementById("snake")!
+    this.moveBody()
   }
 
   // 获取蛇头的坐标
@@ -35,7 +36,25 @@ class Snake{
       // window.confirm("游戏结束")
     }
 
+    // 修改X时是在修改X坐标，蛇在向左移动时不能向右移动，反之亦然
+    if(this.bodies[1] && (this.bodies[1] as HTMLElement).offsetLeft === value){
+      // 如果发生了掉头，应该让蛇向反方向继续移动
+      if(value > this.X){
+        // 如果新值value大于旧值X，说明蛇向右走，此时发生掉头，应该使蛇继续向左走
+        value = this.X - 10
+      }else {
+        value = this.X + 10
+      }
+    }
+
+    // 移动身体
+    this.moveBody()
+
     this.head.style.left = value + "px"
+
+
+    // 检查有没有撞到自己
+    this.checkHeadBody()
   }
   set Y(value) {
     if(this.Y === value){
@@ -48,13 +67,63 @@ class Snake{
       throw new Error("蛇撞墙了")
     }
 
+    // 修改Y时是在修改Y坐标，蛇在向上移动时不能向下移动，反之亦然
+    if(this.bodies[1] && (this.bodies[1] as HTMLElement).offsetTop === value){
+      // 如果发生了掉头，应该让蛇向反方向继续移动
+      if(value > this.Y){
+        // 如果新值value大于旧值Y，说明蛇向右走，此时发生掉头，应该使蛇继续向左走
+        value = this.Y - 10
+      }else {
+        value = this.Y + 10
+      }
+    }
+
+    // 移动身体
+    this.moveBody()
+
     this.head.style.top = value + "px"
+    
+    
+    // 检查有没有撞到自己
+    this.checkHeadBody()
   }
 
   // 设置蛇增加身体的方法
   addBody() {
     // 向element中添加一个div
     this.element.insertAdjacentHTML("beforeend", "<div></div>")
+  }
+
+  // 添加一个蛇身体移动的方法
+  moveBody() {
+    
+    /**
+     * 将后边的身体设置为前边身体的位置
+     */
+
+    // 遍历获取所有的身体
+    for(let i = this.bodies.length - 1; i > 0; i --){
+    // 获取前边身体的位置
+      let X = (this.bodies[i-1] as HTMLElement).offsetLeft;
+      let Y = (this.bodies[i-1] as HTMLElement).offsetTop;
+
+      // 将值设置到当前身体上
+      (this.bodies[i] as HTMLElement).style.left = X + "px";
+      (this.bodies[i] as HTMLElement).style.top = Y + "px";
+    }
+  }
+
+
+  // 检查身体是否撞自己
+  checkHeadBody() {
+    // 获取所有的身体，检查其是否和蛇头的坐标发生重叠
+    for(let i = 1; i < this.bodies.length; i ++){
+      let bd = this.bodies[i] as HTMLElement
+      if(this.X === bd.offsetLeft && this.Y === bd.offsetTop){
+        // 说明蛇头撞到了身体
+        throw new Error("撞到自己了~~");
+      }
+    }
   }
 }
 
